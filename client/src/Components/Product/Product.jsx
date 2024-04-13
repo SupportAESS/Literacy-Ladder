65d5e903e55e325f97bbffec4ea57675fdba7935
Abhinay Verma <aman.ver56@gmail.com>
2024-04-13 18:41:39 +0530

    Display product on main screen

--- client/src/Components/Product/Product.jsx
+++ client/src/Components/Product/Product.jsx
@@ -1,27 +1,32 @@
-import { React, useEffect, useState } from 'react'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React, { useEffect, useState } from 'react';
 import Slider from "react-slick";
 import axios from 'axios';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Product() {
   const [productsByGenre, setProductsByGenre] = useState({});
+  const [hoveredProduct, setHoveredProduct] = useState(null);
+  const [isHovering, setIsHovering] = useState(false);
   const genres = ["Fiction", "Action and Adventure", "Mystery", "Science Fiction", "Fantasy", "Horror", "Biography", "Auto-biography", "History", "Self-help", "Science", "Romance"];
 
   useEffect(() => {
     async function fetchData() {
       try {
         const genreData = {};
-        for (const genre of genres) {
+        const genrePromises = genres.map(async (genre) => {
           const search = {
             "searchBy": "BookGenre",
             "genre": genre
-          }
+          };
           const response = await axios.get("http://localhost:2211/viewBook", {
             params: search
-          })
-          genreData[genre] = response.data;
-        }
+          });
+          if (response.data.length > 0) {
+            genreData[genre] = response.data;
+          }
+        });
+        await Promise.all(genrePromises);
         setProductsByGenre(genreData);
       } catch (error) {
         console.error(error);
@@ -30,64 +35,67 @@ function Product() {
     fetchData();
   }, []);
 
-  var settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    initialSlide: 0,
-    vertical: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+  const handleHover = (product) => {
+    setHoveredProduct(product);
+    setIsHovering(true);
   };
-  
+
+  const handleLeave = () => {
+    setIsHovering(false);
+  };
+
   return (
     <>
-      {genres.map(genre => (
+      {Object.keys(productsByGenre).map(genre => (
         <div key={genre}>
           <h2 className='text-4xl font-bold'>{genre}</h2>
           <div className='w-11/12 mx-auto'>
             <div className='mt-10 mb-10'>
-              <Slider {...settings}>
-                {productsByGenre[genre] && productsByGenre[genre].map(product => (
-                  <div className='bg-white text-black rounded-xl h-auto w-80 overflow-hidden' key={product.bookName}>
-                    <div className='h-60 flex justify-center items-center'>
-                      <img src={product.bookImage} alt="" className='rounded-t-xl w-full h-full' />
-                    </div>
-                    <div className='flex flex-col justify-center items-center gap-4 p-4 h-48'>
-                      <div className="overflow-y-auto h-full">
+              <Slider
+                dots
+                infinite={false}
+                speed={500}
+                slidesToShow={5}
+                slidesToScroll={1}
+                responsive={[
+                  {
+                    breakpoint: 1024,
+                    settings: {
+                      slidesToShow: 3,
+                      slidesToScroll: 3
+                    }
+                  },
+                  {
+                    breakpoint: 600,
+                    settings: {
+                      slidesToShow: 2,
+                      slidesToScroll: 2
+                    }
+                  },
+                  {
+                    breakpoint: 480,
+                    settings: {
+                      slidesToShow: 1,
+                      slidesToScroll: 1
+                    }
+                  }
+                ]}
+              >
+                {productsByGenre[genre].map(product => (
+                  <div
+                    key={product.bookName}
+                    className='relative bg-white text-black rounded-xl overflow-hidden h-80'
+                    onMouseEnter={() => handleHover(product)}
+                    onMouseLeave={() => handleLeave()}
+                  >
+                    <img src={product.bookImage} alt="" className='w-full h-full' />
+                    {isHovering && hoveredProduct === product && (
+                      <div className="absolute bottom-0 left-0 right-0 bg-white text-black p-4">
                         <p className='text-xl font-semibold'>{product.bookName}</p>
-                      </div>
-                      <div className="overflow-y-auto h-full">
                         <p className="text-sm">{product.bookDescription}</p>
+                        <button className='bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl mt-2'>Add to Cart</button>
                       </div>
-                      <button className='bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl'>Add to Card</button>
-                    </div>
+                    )}
                   </div>
                 ))}
               </Slider>
@@ -96,7 +104,7 @@ function Product() {
         </div>
       ))}
     </>
-  )
-};
+  );
+}
 
 export default Product;
