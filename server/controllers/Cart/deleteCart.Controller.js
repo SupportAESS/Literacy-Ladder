0699704aa664557a2c492031d3c19a0263c70865
Abhinay Verma <aman.ver56@gmail.com>
2024-04-17 12:18:49 +0530

    fixed delete

--- server/controllers/Cart/deleteCart.Controller.js
+++ server/controllers/Cart/deleteCart.Controller.js
@@ -1,19 +1,16 @@
 const { Cart } = require("../../models/userModel")
 
 const deleteItem = async (req, res) => {
-    const { userId, book } = req.body;
+    const { userId, item } = req.body;
 
     try {
         // Delete the cart item
         // console.log("Inside");
-        //console.log(req.body);
-        //const search = await Cart.findOne({userId:userId});
-        //console.log(search);
-        const response = await Cart.findOneAndUpdate(
-            {userId: userId}, 
-            { $pull: { cartItems: { book: book } } });
-        console.log(response);
-        console.log("Completed");
+        // console.log(item.book);
+        // console.log(userId);
+        const response = await Cart.updateOne({userId:userId}, { $pull: { cartItems: { book: item.book} } });
+        //console.log(response);
+        //console.log("Completed");
         return res.status(200).json({ message: 'Cart item deleted successfully' });
     } catch (error) {
         console.error('Error deleting cart item:', error);
